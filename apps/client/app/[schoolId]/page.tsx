affea5c03abda4bcc0c224ae62dc106170c272c7
ohchanju <ohchanju3@naver.com>
2026-03-20 05:31:12 +0900

    feat: modal component

--- apps/client/app/[schoolId]/page.tsx
+++ apps/client/app/[schoolId]/page.tsx
@@ -1,13 +1,19 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "components";
 import { testPageStyles as s } from "./TestPage.styles";
+import { Modal } from "@/components/common/Modal/Modal";
 
 export default function TestPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className={s.wrapper}>
       <h1 className="text-head1 text-strong mb-6">두록(DoLog) 테마 테스트</h1>
 
       <div className="flex flex-wrap gap-4">
-        <Button>학교 메인 버튼</Button>
+        <Button onClick={() => setIsModalOpen(true)}>모달 열기</Button>
 
         <Button variant="secondary" size="md">
           보조 버튼
@@ -20,6 +26,29 @@ export default function TestPage() {
         <Button size="sm">작은 버튼</Button>
         <Button size="lg">큰 버튼</Button>
       </div>
+
+      {/* 모달 사용 예시 */}
+      <Modal
+        open={isModalOpen}
+        onOpenChange={setIsModalOpen}
+        title="모달입니다."
+        description={`모발 상세 설명을 작성해주세요.\n 두줄까지 작성하면 보기 좋습니다.`}
+        actions={[
+          {
+            text: "Button",
+            onClick: () => setIsModalOpen(false),
+            variant: "secondary",
+          },
+          {
+            text: "Button",
+            onClick: () => {
+              alert("확인되었습니다!");
+              setIsModalOpen(false);
+            },
+            variant: "primary",
+          },
+        ]}
+      />
     </div>
   );
 }
