bfe86581045bb7929d379b28725d70991530a356
leeboyeon17 <2023110695@dgu.ac.kr>
2026-03-25 13:25:01 +0900

    feat: apps/client 패키지 연결 설정 및 기존 타입 에러 수정

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ["components", "api", "utils"],
 };
 
 export default nextConfig;
