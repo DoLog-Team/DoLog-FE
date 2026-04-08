e009b6af7b4d0c3b84e6b47ad47b80d1dad4c8bb
ohchanju <ohchanju3@naver.com>
2026-04-09 02:19:59 +0900

    fix: card.tsx import error 해결

--- apps/client/components/common/Card/Card.tsx
+++ apps/client/components/common/Card/Card.tsx
@@ -1,22 +1,17 @@
 import { cardStyles as s } from "./Card.styles";
-import { CardProps } from "./Card.types";
+import type { CardProps } from "./Card.types";
 
 export const Card = ({ imageUrl, title, category, author }: CardProps) => {
-  return (
-    <article className={s.wrapper}>
-      <div className={s.imageWrapper}>
-        <img
-          src={imageUrl}
-          alt={`${title} - ${author}`}
-          className={s.image}
-          loading="lazy"
-        />
-      </div>
-      <div className={s.info}>
-        <h3 className={s.title}>{title}</h3>
-        <p className={s.category}>{category}</p>
-        <p className={s.author}>{author}</p>
-      </div>
-    </article>
-  );
+	return (
+		<article className={s.wrapper}>
+			<div className={s.imageWrapper}>
+				<img src={imageUrl} alt={`${title} - ${author}`} className={s.image} loading="lazy" />
+			</div>
+			<div className={s.info}>
+				<h3 className={s.title}>{title}</h3>
+				<p className={s.category}>{category}</p>
+				<p className={s.author}>{author}</p>
+			</div>
+		</article>
+	);
 };
