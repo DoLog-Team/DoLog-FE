8033c08774a4aab60ba28ebd26d3e33948db3b5c
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2026-04-23 23:58:49 +0900

    feat: Title 폰트 사이즈 설정 기능 구현

--- apps/client/components/common/Title/Title.styles.ts
+++ apps/client/components/common/Title/Title.styles.ts
@@ -1,17 +1,27 @@
 import { cva } from "class-variance-authority";
 
 export const titleLayoutStyles = {
-  wrapper: cva("w-full flex flex-col", {
-    variants: {
-      margin: {
-        default: "mt-[20px] mb-[12px]",
-        compact: "mt-[16px] mb-[16px]",
-      },
-    },
-    defaultVariants: {
-      margin: "default",
-    },
-  }),
+	wrapper: cva("w-full flex flex-col", {
+		variants: {
+			margin: {
+				default: "mt-[20px] mb-[12px]",
+				compact: "mt-[16px] mb-[16px]",
+			},
+		},
+		defaultVariants: {
+			margin: "default",
+		},
+	}),
 
-  text: "text-strong text-head1",
+	text: cva("text-strong", {
+		variants: {
+			size: {
+				head1: "text-head1",
+				head2: "text-head2",
+			},
+		},
+		defaultVariants: {
+			size: "head1",
+		},
+	}),
 };
