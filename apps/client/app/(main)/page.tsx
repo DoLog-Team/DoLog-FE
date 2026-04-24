17ef28e08bc00462e89e6d0e0b07615634a86824
ohchanju <ohchanju3@naver.com>
2026-04-24 20:14:23 +0900

    feat: 메인홈 섹션 (진행중인 전시회, 기타 섹션) ui 구현

--- apps/client/app/(main)/page.tsx
+++ apps/client/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { Title } from "@/components/common/Title/Title";
 import MainFooter from "@/components/common/Footer/MainFooter";
+import { Title } from "@/components/common/Title/Title";
 import Banner from "./_components/Banner";
 import CategorySection from "./_components/CategorySection";
 import ExhibitionCard from "./_components/ExhibitionCard";
@@ -10,44 +10,44 @@ import { MOCK_EXHIBITIONS } from "./_mocks/exhibition";
 import { MOCK_SECTIONS } from "./_mocks/section";
 
 export default function MainPage() {
-  return (
-    <div className="flex flex-col">
-      <Header />
-      <Banner banners={MOCK_BANNERS} />
+	return (
+		<div className="flex flex-col">
+			<Header />
+			<Banner banners={MOCK_BANNERS} />
 
-      <section className="flex flex-col px-4 pt-6">
-        <Title title="진행중인 전시회" />
-        <div className="flex flex-col gap-4 mt-4">
-          {MOCK_EXHIBITIONS.map((exhibition) => (
-            <Link
-              key={exhibition.id}
-              href={`/exhibition/${exhibition.id}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <ExhibitionCard {...exhibition} />
-            </Link>
-          ))}
-        </div>
-        <Link
-          href="/exhibitions"
-          className="bg-fg-lighter text-element1 text-light text-center py-4 rounded-lg mt-7"
-        >
-          더보기
-        </Link>
-      </section>
+			<section className="flex flex-col px-4 pt-6">
+				<Title title="진행중인 전시회" />
+				<div className="flex flex-col gap-4 mt-4">
+					{MOCK_EXHIBITIONS.map((exhibition) => (
+						<Link
+							key={exhibition.id}
+							href={`/exhibition/${exhibition.id}`}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<ExhibitionCard {...exhibition} />
+						</Link>
+					))}
+				</div>
+				<Link
+					href="/exhibitions"
+					className="bg-fg-lighter text-element1 text-light text-center py-4 rounded-lg mt-7"
+				>
+					더보기
+				</Link>
+			</section>
 
-      {MOCK_SECTIONS.map((section) => (
-        <div key={section.title}>
-          {/* TODO: Divider 컴포넌트 머지 후 추가 */}
-          <CategorySection
-            title={section.title}
-            categories={section.categories}
-            artworks={section.artworks}
-          />
-        </div>
-      ))}
-      <MainFooter />
-    </div>
-  );
+			{MOCK_SECTIONS.map((section) => (
+				<div key={section.title}>
+					{/* TODO: Divider 컴포넌트 머지 후 추가 */}
+					<CategorySection
+						title={section.title}
+						categories={section.categories}
+						artworks={section.artworks}
+					/>
+				</div>
+			))}
+			<MainFooter />
+		</div>
+	);
 }
--- apps/client/components/common/Footer/MainFooter.tsx
+++ apps/client/components/common/Footer/MainFooter.tsx
@@ -1,20 +1,18 @@
 import Image from "next/image";
 
 export default function MainFooter() {
-  return (
-    <footer className="w-full pt-12 pb-12 flex flex-col bg-fg-lighter mt-12">
-      <div className="flex flex-col px-4">
-        <Image src="/images/logo.svg" alt="DoLog Logo" width={50} height={25} />
-        <div className="h-5 w-full" />
-        <div className="flex flex-col gap-1">
-          <p className="text-body2 text-lighter">전시를 다르게 보다</p>
-          <p className="text-body2 text-lighter">example@example.com</p>
-        </div>
-        <div className="h-3 w-full" />
-        <p className="text-body3-bold text-lightest ">
-          Website powered by DOLOG
-        </p>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="w-full pt-12 pb-12 flex flex-col bg-fg-lighter mt-12">
+			<div className="flex flex-col px-4">
+				<Image src="/images/logo.svg" alt="DoLog Logo" width={50} height={25} />
+				<div className="h-5 w-full" />
+				<div className="flex flex-col gap-1">
+					<p className="text-body2 text-lighter">전시를 다르게 보다</p>
+					<p className="text-body2 text-lighter">example@example.com</p>
+				</div>
+				<div className="h-3 w-full" />
+				<p className="text-body3-bold text-lightest ">Website powered by DOLOG</p>
+			</div>
+		</footer>
+	);
 }
