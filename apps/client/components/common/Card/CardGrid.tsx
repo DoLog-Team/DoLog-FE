af70f0b2a018753ea0ab0c95a57a064e6d16e6b7
ohchanju <ohchanju3@naver.com>
2026-04-26 00:00:21 +0900

    feat: 전체 작품 리스트 페이지 구현

--- apps/client/components/common/Card/CardGrid.tsx
+++ apps/client/components/common/Card/CardGrid.tsx
@@ -1,14 +1,21 @@
+import Link from "next/link";
 import { Card } from "./Card";
 import type { CardGridProps } from "./Card.types";
 
-export const CardGrid = ({ items, limit }: CardGridProps) => {
+export const CardGrid = ({ items, limit, getHref }: CardGridProps) => {
 	const displayedItems = limit ? items.slice(0, limit) : items;
 
 	return (
 		<div className="grid grid-cols-2 gap-x-4 gap-y-6 w-full">
-			{displayedItems.map((item) => (
-				<Card key={item.id} {...item} />
-			))}
+			{displayedItems.map((item) =>
+				getHref ? (
+					<Link key={item.id} href={getHref(item)}>
+						<Card {...item} />
+					</Link>
+				) : (
+					<Card key={item.id} {...item} />
+				),
+			)}
 		</div>
 	);
 };
