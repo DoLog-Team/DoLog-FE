04d4f5dd64c63e0ad85d63a52db415a13dff9ccf
ohchanju <ohchanju3@naver.com>
2026-04-26 00:00:41 +0900

    style: 린트 자동 수정

--- apps/client/app/(main)/artworks/page.tsx
+++ apps/client/app/(main)/artworks/page.tsx
@@ -4,9 +4,7 @@ import ArtworksClient from "./_components/ArtworksClient";
 const categories = CATEGORIES.filter((c) => c !== "전체");
 
 const allArtworks = [
-	...new Map(
-		categories.flatMap((c) => MOCK_ARTWORKS[c]).map((item) => [item.id, item]),
-	).values(),
+	...new Map(categories.flatMap((c) => MOCK_ARTWORKS[c]).map((item) => [item.id, item])).values(),
 ];
 
 export default function ArtworksPage() {
