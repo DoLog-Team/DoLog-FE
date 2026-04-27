492884da95c4c26135fb7a0362d53721d2be61a0
leeboyeon17 <2023110695@dgu.ac.kr>
2026-04-27 22:20:52 +0900

    feat: 보기방식 토글 분기 처리

--- apps/client/app/[schoolId]/exhibition/[exhibitionId]/artwork/_components/ArtworkListSection.tsx
+++ apps/client/app/[schoolId]/exhibition/[exhibitionId]/artwork/_components/ArtworkListSection.tsx
@@ -1,50 +1,40 @@
 "use client";
+import Image from "next/image";
 import { useState } from "react";
-import { CATEGORIES, MOCK_ARTWORKS } from "@/app/(main)/_mocks/artwork";
 import type { Category } from "@/app/(main)/_mocks/artwork";
+import { CATEGORIES, MOCK_ARTWORKS } from "@/app/(main)/_mocks/artwork";
 import { CardGrid } from "@/components/common/Card/CardGrid";
 import { ListCardGrid } from "@/components/common/Card/ListCard/ListCardGrid";
 import { SearchBar } from "@/components/common/SearchBar/SearchBar";
 import { Title } from "@/components/common/Title/Title";
 import Filter from "./components/Filter";
-import Image from "next/image";
+import { ListIcon } from "./components/ListIcon";
+import { AlbumIcon } from "./components/AlbumIcon";
 
 export function ArtworkListSection() {
-
 	const [selected, setSelected] = useState<Category>("전체");
-    const items = MOCK_ARTWORKS[selected];
+	const items = MOCK_ARTWORKS[selected];
 	const [viewMode, setViewMode] = useState<"grid" | "list">("grid");
 
 	return (
 		<section className="flex flex-col px-4 pb-6">
 			<div className="flex justify-between items-center">
-			<Title title="작품 목록" />
-			{/* 보기 방식 토글 버튼 */}
-			<div className="flex gap-1 p-1 h-8 rounded-2 border border-stroke-lightest">
-                <Image src="/icons/list.svg" alt="리스트보기" width={24} height={24}
-					onClick={() => setViewMode("list")}
-       				className="cursor-pointer" 
-				/>
-                <div className="w-px h-full border border-stroke-lighter" />
-                <Image src="/icons/album.svg" alt="갤러리보기" width={24} height={24}
-					onClick={() => setViewMode("grid")}
-       				className="cursor-pointer" 
-				/>
-            </div>
+				<Title title="작품 목록" />
+				{/* 보기 방식 토글 버튼 */}
+				<div className="flex gap-1 p-1 h-8 rounded-2 border border-stroke-lightest">
+					<ListIcon active={viewMode === "list"} onClick={() => setViewMode("list")} className="cursor-pointer" />
+					<div className="w-px h-full border border-stroke-lighter" />
+					<AlbumIcon active={viewMode === "grid"} onClick={() => setViewMode("grid")} className="cursor-pointer" />
+				</div>
 			</div>
-			
+
 			<SearchBar placeholder="작품명, 작가명을 검색하세요" />
-			<Filter 
-			categories={CATEGORIES}
-                selected={selected}
-                onSelect={(category) => setSelected(category)}
-			
+			<Filter
+				categories={CATEGORIES}
+				selected={selected}
+				onSelect={(category) => setSelected(category)}
 			/>
-			{viewMode === "grid" ? (
-    <CardGrid items={items} />
-) : (
-    <ListCardGrid items={items} />
-)}
+			{viewMode === "grid" ? <CardGrid items={items} /> : <ListCardGrid items={items} />}
 		</section>
 	);
 }
--- apps/client/app/[schoolId]/exhibition/[exhibitionId]/artwork/_components/components/Filter.tsx
+++ apps/client/app/[schoolId]/exhibition/[exhibitionId]/artwork/_components/components/Filter.tsx
@@ -1,25 +1,24 @@
-import { Chip } from "@/components/common/Chip/Chip";
 import type { Category } from "@/app/(main)/_mocks/artwork";
-
+import { Chip } from "@/components/common/Chip/Chip";
 
 interface FilterProps {
-    categories: readonly Category[];
-    selected: Category;
-    onSelect: (category: Category) => void;
+	categories: readonly Category[];
+	selected: Category;
+	onSelect: (category: Category) => void;
 }
 
 export default function Filter({ categories, selected, onSelect }: FilterProps) {
-    return (
-        <div className="flex items-center mt-2.5 mb-6 gap-2.5 overflow-x-auto scrollbar-hide">
-            {categories.map((category) => (
-                <Chip
-                    key={category}
-                    label={category}
-                    type={category === "전체" ? "custom" : "assistive"}
-                    selected={selected === category}
-                    onClick={() => onSelect(category)}
-                />
-            ))}            
-        </div>
-    )
+	return (
+		<div className="flex items-center mt-2.5 mb-6 gap-2.5 overflow-x-auto scrollbar-hide">
+			{categories.map((category) => (
+				<Chip
+					key={category}
+					label={category}
+					type={category === "전체" ? "custom" : "assistive"}
+					selected={selected === category}
+					onClick={() => onSelect(category)}
+				/>
+			))}
+		</div>
+	);
 }
