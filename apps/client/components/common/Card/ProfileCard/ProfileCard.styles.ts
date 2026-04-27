468973999903f169bdee54f85913e822d40da3d2
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2026-04-27 13:25:41 +0900

    refactor: ProfileCard 컴포넌트 스타일 분리 및 리팩토링

--- apps/client/components/common/Card/ProfileCard/ProfileCard.styles.ts
+++ apps/client/components/common/Card/ProfileCard/ProfileCard.styles.ts
@@ -1,7 +1,7 @@
 export const profileCardStyles = {
 	wrapper: "flex flex-col gap-4 w-full pt-4",
 
-	top: "flex gap-3",
+	top: "flex gap-[12px]",
 
 	image: "w-[150px] h-[200px] object-cover",
 
@@ -10,7 +10,7 @@ export const profileCardStyles = {
 	textInner: "flex flex-col mt-auto",
 
 	name: "text-head3",
-	engName: "text-body2 text-light mt-1",
+	engName: "text-body2 text-light mt-[4px]",
 
-	bio: "text-body1 text-light mt-2",
+	bio: "text-body1 text-light leading-[24px] mt-2",
 };
