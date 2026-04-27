05a0c1ba0cf3b6b25315c8ddb1feff9c969925cc
ohchanju <ohchanju3@naver.com>
2026-04-27 23:03:40 +0900

    style: 린트 자동 수정

--- apps/client/app/[schoolId]/exhibition/[exhibitionId]/bts/page.tsx
+++ apps/client/app/[schoolId]/exhibition/[exhibitionId]/bts/page.tsx
@@ -1,5 +1,5 @@
-import { MOCK_BEHIND_THE_SCENE } from "./_mocks/behind-the-scene";
 import BehindTheSceneClient from "./_components/BehindTheSceneClient";
+import { MOCK_BEHIND_THE_SCENE } from "./_mocks/behind-the-scene";
 
 export default function BtsPage() {
 	return <BehindTheSceneClient items={MOCK_BEHIND_THE_SCENE} />;
