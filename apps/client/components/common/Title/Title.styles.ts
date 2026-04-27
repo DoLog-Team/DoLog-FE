0f386aa2f58080f961f71b28aff16ce1cd8cd758
ohchanju <ohchanju3@naver.com>
2026-04-28 04:08:34 +0900

    style: 린트 자동 수정

--- apps/client/components/common/Title/Title.styles.ts
+++ apps/client/components/common/Title/Title.styles.ts
@@ -1,28 +1,28 @@
 import { cva } from "class-variance-authority";
 
 export const titleStyles = cva("w-full flex flex-col", {
-  variants: {
-    margin: {
-      default: "mt-[20px] mb-[12px]",
-      compact: "mt-[16px] mb-[16px]",
-      none: "m-0",
-    },
-    size: {
-      head1: "text-head1",
-      head2: "text-head2",
-      body1: "text-body1",
-      "body1-bold": "text-body1-bold",
-      body2: "text-body2",
-    },
-    color: {
-      strong: "text-strong",
-      light: "text-light",
-      lighter: "text-lighter",
-    },
-  },
-  defaultVariants: {
-    margin: "default",
-    size: "head1",
-    color: "strong",
-  },
+	variants: {
+		margin: {
+			default: "mt-[20px] mb-[12px]",
+			compact: "mt-[16px] mb-[16px]",
+			none: "m-0",
+		},
+		size: {
+			head1: "text-head1",
+			head2: "text-head2",
+			body1: "text-body1",
+			"body1-bold": "text-body1-bold",
+			body2: "text-body2",
+		},
+		color: {
+			strong: "text-strong",
+			light: "text-light",
+			lighter: "text-lighter",
+		},
+	},
+	defaultVariants: {
+		margin: "default",
+		size: "head1",
+		color: "strong",
+	},
 });
--- apps/client/components/common/Title/Title.tsx
+++ apps/client/components/common/Title/Title.tsx
@@ -2,23 +2,13 @@ import { cn } from "components";
 import { titleStyles } from "./Title.styles";
 
 interface TitleLayoutProps {
-  title: string;
-  margin?: "default" | "compact" | "none";
-  size?: "head1" | "head2" | "body1-bold" | "body1" | "body2";
-  color?: "strong" | "light" | "lighter";
-  className?: string;
+	title: string;
+	margin?: "default" | "compact" | "none";
+	size?: "head1" | "head2" | "body1-bold" | "body1" | "body2";
+	color?: "strong" | "light" | "lighter";
+	className?: string;
 }
 
-export const Title = ({
-  title,
-  margin,
-  size,
-  color,
-  className,
-}: TitleLayoutProps) => {
-  return (
-    <h2 className={cn(titleStyles({ margin, size, color }), className)}>
-      {title}
-    </h2>
-  );
+export const Title = ({ title, margin, size, color, className }: TitleLayoutProps) => {
+	return <h2 className={cn(titleStyles({ margin, size, color }), className)}>{title}</h2>;
 };
