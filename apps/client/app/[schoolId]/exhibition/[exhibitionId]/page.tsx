33acbf5e6b73b609841ecc7f9a673d3b5b607213
ohchanju <ohchanju3@naver.com>
2026-04-29 15:18:04 +0900

    feat: generateMetadata 생성 - 메타 데이터 동적

--- apps/client/app/[schoolId]/exhibition/[exhibitionId]/page.tsx
+++ apps/client/app/[schoolId]/exhibition/[exhibitionId]/page.tsx
@@ -1,5 +1,7 @@
+import type { Metadata } from "next";
 import Image from "next/image";
 import { MOCK_EXHIBITION_DATA } from "@/constants/exhibition";
+import { getExhibitionMeta } from "./_api/getExhibitionMeta";
 import { ExhibitionDetail } from "./_components/ExhibitionDetailSection";
 import { ExhibitionHost } from "./_components/ExhibitionHostSection";
 import { ExhibitionIntro } from "./_components/ExhibitionIntroSection";
@@ -7,38 +9,60 @@ import { ExhibitionLocation } from "./_components/ExhibitionLocationSection";
 import { Header } from "./_components/Header";
 
 interface ExhibitionDetailPageProps {
-	params: Promise<{ schoolId: string; exhibitionId: string }>;
+  params: Promise<{ schoolId: string; exhibitionId: string }>;
 }
 
-export default async function ExhibitionDetailPage({ params }: ExhibitionDetailPageProps) {
-	const { exhibitionId } = await params;
-	const exhibition = MOCK_EXHIBITION_DATA.find((e) => e.id === exhibitionId);
-	if (!exhibition) {
-		return <div>전시회를 찾을 수 없습니다.</div>;
-	}
+export async function generateMetadata({
+  params,
+}: ExhibitionDetailPageProps): Promise<Metadata> {
+  const { exhibitionId } = await params;
+  const meta = await getExhibitionMeta(exhibitionId);
 
-	return (
-		<main>
-			<Header />
-			{/* 대표 이미지 */}
-			<div className="relative w-full aspect-3/4">
-				<Image
-					src={exhibition.thumbnailUrl}
-					alt={exhibition.title}
-					fill
-					className="object-cover"
-					priority
-				/>
-			</div>
-			{/* 제목 및 기본 정보 */}
-			<ExhibitionIntro exhibition={exhibition} />
-			{/* 전시 소개 */}
-			<ExhibitionDetail exhibition={exhibition} />
-			{/* TODO : 머지 후 Divider import */}
-			{/* 장소 */}
-			<ExhibitionLocation location={exhibition.location} />
-			{/* 주최 기관 */}
-			<ExhibitionHost hostInfo={exhibition.hostInfo} socialLinks={exhibition.socialLinks} />
-		</main>
-	);
+  return {
+    title: meta?.title,
+    description: meta?.description,
+    openGraph: {
+      title: meta?.title,
+      description: meta?.description ?? undefined,
+      images: meta?.image ? [{ url: meta.image }] : [],
+    },
+  };
+}
+
+export default async function ExhibitionDetailPage({
+  params,
+}: ExhibitionDetailPageProps) {
+  const { exhibitionId } = await params;
+  const exhibition = MOCK_EXHIBITION_DATA.find((e) => e.id === exhibitionId);
+  if (!exhibition) {
+    return <div>전시회를 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <main>
+      <Header />
+      {/* 대표 이미지 */}
+      <div className="relative w-full aspect-3/4">
+        <Image
+          src={exhibition.thumbnailUrl}
+          alt={exhibition.title}
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+      {/* 제목 및 기본 정보 */}
+      <ExhibitionIntro exhibition={exhibition} />
+      {/* 전시 소개 */}
+      <ExhibitionDetail exhibition={exhibition} />
+      {/* TODO : 머지 후 Divider import */}
+      {/* 장소 */}
+      <ExhibitionLocation location={exhibition.location} />
+      {/* 주최 기관 */}
+      <ExhibitionHost
+        hostInfo={exhibition.hostInfo}
+        socialLinks={exhibition.socialLinks}
+      />
+    </main>
+  );
 }
