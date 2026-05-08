fe13eca333041de39693c328e5136c3f7d5ada88
Yeonhee-Ha <78415838+Yeonhee-Ha@users.noreply.github.com>
2026-05-09 04:05:53 +0900

    refactor: 아티스트 관련 API 폴더 분리

---
+++ apps/client/lib/api/artists/artist-detail.types.ts
@@ -0,0 +1,42 @@
+export interface ArtistSns {
+	snsId: string;
+	platformName: string;
+	url: string;
+}
+
+export interface ArtistContact {
+	email: string;
+	sns: ArtistSns[];
+}
+
+export interface ArtistBts {
+	btsId: string;
+	title: string;
+	mainImg: string;
+}
+
+export interface ArtistArtwork {
+	artworkId: string;
+	title: string;
+	image: string;
+}
+
+export interface ArtistNavItem {
+	id: string;
+	name: string;
+}
+
+export interface ArtistDetail {
+	profileId: string;
+	artistId: string;
+	nameKo: string;
+	nameEn: string;
+	profileImage: string;
+	bio: string;
+	contact: ArtistContact;
+	behindTheScenes: ArtistBts[];
+	artworks: ArtistArtwork[];
+	prevArtist?: ArtistNavItem;
+	nextArtist?: ArtistNavItem;
+	public: boolean;
+}
