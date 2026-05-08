fefe4aa41afce311da00cf26c817ef43276b1ea3
leeboyeon17 <2023110695@dgu.ac.kr>
2026-05-08 23:14:56 +0900

    feat: footer api 연결

--- apps/client/app/[exhibitionId]/layout.tsx
+++ apps/client/app/[exhibitionId]/layout.tsx
@@ -1,6 +1,17 @@
 import SchoolFooter from "@/components/common/Footer/SchoolFooter";
 import { ThemeProvider } from "@/providers/theme-providers";
 import { DEFAULT_EXHIBITION_CONFIG, MOCK_EXHIBITION_CONFIG } from "./exhibition-config";
+import { getExhibitionFooter } from "@/lib/api/layout";
+import { resolveExhibitionId } from "./_api/resolveExhibitionId";
+
+const MOCK_FOOTER = {
+  title: "흙에서 시작되는 모든 이야기",
+  department: "한국대학교 예술대학 도예과",
+  address: "서울 중구 필동로1길 30",
+  address_detail: "동국대학교 문화관 지하 1층 동국갤러리",
+  email: "dgu_art@dongguk.edu",
+  copyright: "©2025. Dongguk University Sculpture Department Exhibition.",
+};
 
 export default async function ExhibitionLayout({
 	children,
@@ -10,7 +21,12 @@ export default async function ExhibitionLayout({
 	params: Promise<{ exhibitionId: string }>;
 }) {
 	const { exhibitionId } = await params;
+	const uuid = await resolveExhibitionId(exhibitionId);
 	const config = MOCK_EXHIBITION_CONFIG[exhibitionId] ?? DEFAULT_EXHIBITION_CONFIG;
+
+	const footer = uuid
+	? (await getExhibitionFooter(uuid)) ?? MOCK_FOOTER
+	: MOCK_FOOTER;
 
 	const colorVars = {
 		...(config.btnBg && { "--btn-bg": config.btnBg }),
@@ -34,7 +50,16 @@ export default async function ExhibitionLayout({
 			<div className="bg-normal text-strong min-h-dvh flex flex-col" style={colorVars}>
 				<div className="min-h-dvh flex flex-col w-full max-w-135 mx-auto">{children}</div>
 
-				<SchoolFooter logoSrc={config.footerInfo.logoSrc} {...config.footerInfo} />
+				{/* <SchoolFooter logoSrc={config.footerInfo.logoSrc} {...config.footerInfo} /> */}
+				<SchoolFooter
+					logoSrc={config.footerInfo.logoSrc}
+					title={footer.title}
+					department={footer.department}
+					address={footer.address ?? ""}
+					addressDetail={footer.address_detail ?? ""}
+					email={footer.email}
+					copyright={footer.copyright ?? ""}
+				/>
 			</div>
 		</ThemeProvider>
 	);
--- apps/client/lib/api/layout.ts
+++ apps/client/lib/api/layout.ts
@@ -1,20 +1,22 @@
 import { apiClient } from "api";
 
-/**
+/*******************
  * Footer 정보 조회
- */
+ * GET exhibition/{id}/footer-info
+ *******************/
+
 export interface ExhibitionFooter {
-	title: string;
-	department: string;
-	address: string;
-	address_detail: string;
-	email: string;
-	copyright: string;
+  title: string;
+  department: string;
+  address: string | null;
+  address_detail: string | null;
+  email: string;
+  copyright: string | null;
 }
 
 export async function getExhibitionFooter(exhibitionId: string): Promise<ExhibitionFooter | null> {
 	try {
-		return await apiClient<ExhibitionFooter>(`/exhibition/${exhibitionId}/footer-info`);
+		return await apiClient<ExhibitionFooter>(`/exhibitions/${exhibitionId}/footer-info`);
 	} catch {
 		return null;
 	}
