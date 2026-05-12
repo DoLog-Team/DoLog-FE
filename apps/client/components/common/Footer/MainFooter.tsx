e6113c521b21a364907564a249ee3967e29e4831
ohchanju <ohchanju3@naver.com>
2026-05-12 23:51:05 +0900

    fix: main footer wording

--- apps/client/components/common/Footer/MainFooter.tsx
+++ apps/client/components/common/Footer/MainFooter.tsx
@@ -7,8 +7,8 @@ export default function MainFooter() {
 				<Image src="/images/logo.svg" alt="DoLog Logo" width={50} height={25} />
 				<div className="h-5 w-full" />
 				<div className="flex flex-col gap-1">
-					<p className="text-body2 text-lighter">전시를 다르게 보다</p>
-					<p className="text-body2 text-lighter">example@example.com</p>
+					<p className="text-body2 text-lighter">우리의 졸업 전시, 더 오래 기록하는 방법</p>
+					<p className="text-body2 text-lighter">dologarchive@gmail.com</p>
 				</div>
 				<div className="h-3 w-full" />
 				<p className="text-body3-bold text-lightest ">Website powered by DOLOG</p>
