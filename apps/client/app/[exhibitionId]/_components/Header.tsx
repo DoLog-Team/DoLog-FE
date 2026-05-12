fb926489af73c673151545886d3c9fec370702df
ohchanju <ohchanju3@naver.com>
2026-05-12 22:57:42 +0900

    fix: header logo size 코드 롤백

--- apps/client/app/[exhibitionId]/_components/Header.tsx
+++ apps/client/app/[exhibitionId]/_components/Header.tsx
@@ -44,7 +44,7 @@ export const Header = ({ variant = "logo", title }: HeaderProps) => {
 								width={0}
 								height={0}
 								sizes="100vw"
-								className="h-5 w-auto"
+								className="h-16 w-auto"
 								priority
 							/>
 						) : (
