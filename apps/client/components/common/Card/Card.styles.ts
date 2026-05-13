10ea66327766bb16dd7eaf9a8ec5daa8564b9ec2
ohchanju <ohchanju3@naver.com>
2026-05-14 05:05:41 +0900

    fix: 공백 없는 긴 제목 텍스트 줄바꿈 처리

--- apps/client/components/common/Card/Card.styles.ts
+++ apps/client/components/common/Card/Card.styles.ts
@@ -3,7 +3,7 @@ export const cardStyles = {
 	imageWrapper: "relative w-full aspect-[1/1.414] overflow-hidden ",
 	image: "w-full h-full object-cover",
 	info: "w-full flex flex-col gap-[4px]",
-	title: "text-strong text-head3",
+	title: "text-strong text-head3 break-all",
 	category: "text-light text-body2",
 	author: "text-light text-body2",
 };
