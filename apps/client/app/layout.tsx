fadc49de6fe5024341234a6ce040cdc3379ca49d
ohchanju <ohchanju3@naver.com>
2026-05-16 03:19:40 +0900

    fix: og image 수정

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -4,9 +4,10 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
 	title: "두록",
 	description: "우리의 졸업 전시, 더 오래 기록하는 방법",
-	//TODO: 추후 연결 도메인으로 변경 필요
 	metadataBase: new URL("https://dolog.kr"),
 	openGraph: {
+		type: "website",
+		url: "https://dolog.kr",
 		title: "두록",
 		description: "우리의 졸업 전시, 더 오래 기록하는 방법",
 		images: [{ url: "/images/og-default.png" }],
