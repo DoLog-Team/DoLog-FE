2c9b9edaf80eab69eb2b2a9dc0bf2a09de2478cf
ohchanju <ohchanju3@naver.com>
2026-05-17 00:40:19 +0900

    design: 작가 상세 페이지 디자인 간격 조정

--- apps/client/app/[exhibitionId]/artist/[artistId]/page.tsx
+++ apps/client/app/[exhibitionId]/artist/[artistId]/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Divider } from "@/components/common/Divider/Divider";
 import { resolveExhibitionSlug } from "@/lib/api/exhibition";
 import { getArtistDetail } from "../../../../lib/api/artists/artist-detail";
 import { getExhibitionMeta } from "../../_api/getExhibitionMeta";
@@ -55,6 +56,8 @@ export default async function ArtistDetailPage({ params }: Props) {
 				<ProfileSection artist={artist} />
 
 				<ContactSection contact={artist.contact} />
+
+				<Divider />
 
 				<BTSSection exhibitionId={exhibitionId} artist={artist} />
 
