4b8162f5c6a40b9a9c4fd745470ffadae07df0c0
ohchanju <ohchanju3@naver.com>
2026-06-02 04:05:32 +0900

    fix: mock fallback 적용

--- apps/client/app/[exhibitionId]/bts/[btsId]/page.tsx
+++ apps/client/app/[exhibitionId]/bts/[btsId]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { getBtsDetail } from "@/lib/api/bts";
 import { resolveExhibitionId } from "../../_api/resolveExhibitionId";
 import { MOCK_BTS_DETAILS } from "../_mocks/behind-the-scene";
@@ -11,10 +12,14 @@ export default async function BtsDetailPage({ params }: Props) {
 	const { exhibitionId, btsId } = await params;
 	const uuid = await resolveExhibitionId(exhibitionId);
 
+	// TODO: 배포 전 _mocks/behind-the-scene.ts의 MOCK_BTS_DETAILS 주석 처리
 	const btsItem =
 		(uuid ? await getBtsDetail(uuid, btsId) : null) ??
-		MOCK_BTS_DETAILS.find((m) => m.btsId === btsId) ??
-		MOCK_BTS_DETAILS[0];
+		(MOCK_BTS_DETAILS.length > 0
+			? (MOCK_BTS_DETAILS.find((m) => m.btsId === btsId) ?? MOCK_BTS_DETAILS[0])
+			: null);
+
+	if (!btsItem) notFound();
 
 	return <BtsDetailClient btsItem={btsItem} exhibitionId={exhibitionId} />;
 }
