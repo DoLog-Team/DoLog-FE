d8428ddb6d29de70f81cfe7dcb1072cbb917ae0a
ohchanju <ohchanju3@naver.com>
2026-06-02 17:58:33 +0900

    style: 푸터 문구 수정

--- apps/client/components/common/Footer/MainFooter.tsx
+++ apps/client/components/common/Footer/MainFooter.tsx
@@ -7,11 +7,15 @@ export default function MainFooter() {
 				<Image src="/images/logo.svg" alt="DoLog Logo" width={50} height={25} />
 				<div className="h-5 w-full" />
 				<div className="flex flex-col gap-1">
-					<p className="text-body2 text-lighter">우리의 졸업 전시, 더 오래 기록하는 방법</p>
+					<p className="text-body2 text-lighter">
+						두록은 대학 전시를 위한 전시 웹사이트 제작 및 작품 아카이빙 플랫폼입니다.
+					</p>
 					<p className="text-body2 text-lighter">dologarchive@gmail.com</p>
 				</div>
 				<div className="h-3 w-full" />
-				<p className="text-body3-bold text-lightest ">Website powered by DOLOG</p>
+				<p className="text-body3-bold text-lightest">
+					Website powered by 두록(DOLOG) / 대학 전시 웹사이트 제작 및 작품 아카이빙 플랫폼
+				</p>
 			</div>
 		</footer>
 	);
