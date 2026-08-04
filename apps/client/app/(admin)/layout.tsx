a0a7b9df426d158c42e72247dc04fdba18cfcaf3
ohchanju <ohchanju3@naver.com>
2026-08-04 23:22:31 +0900

    chore: admin GA 트래킹 스캐폴딩 추가

--- apps/client/app/(admin)/layout.tsx
+++ apps/client/app/(admin)/layout.tsx
@@ -1,3 +1,13 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-	return <>{children}</>;
+	return (
+		<>
+			{process.env.NEXT_PUBLIC_ADMIN_GA_MEASUREMENT_ID && (
+				<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_ADMIN_GA_MEASUREMENT_ID} />
+			)}
+			{/* 사이드바 + 헤더 shell 구조 */}
+			{children}
+		</>
+	);
 }
