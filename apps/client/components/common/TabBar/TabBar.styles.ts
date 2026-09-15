e99ff2bb8a4c3b231d242442681ec20d9c4a520d
leeboyeon17 <2023110695@dgu.ac.kr>
2026-09-16 02:03:08 +0900

    feat: 이미지 인풋 컴포넌트 구현

--- apps/client/components/common/TabBar/TabBar.styles.ts
+++ apps/client/components/common/TabBar/TabBar.styles.ts
@@ -1,18 +1,18 @@
 import { cva, type VariantProps } from "class-variance-authority";
 
 export const tabItemVariants = cva(
-  "flex items-center gap-[6px] text-body4-bold transition-colors cursor-pointer whitespace-nowrap border-b-2 border-stroke-inverse px-[14px] py-[12px]",
-  {
-    variants: {
-      active: {
-        true: "text-body4-bold text-light",
-        false: "text-body4 text-lightest border-transparent",
-      },
-    },
-    defaultVariants: {
-      active: false,
-    },
-  },
+	"flex items-center gap-[6px] text-body4-bold transition-colors cursor-pointer whitespace-nowrap border-b-2 border-stroke-inverse px-[14px] py-[12px]",
+	{
+		variants: {
+			active: {
+				true: "text-body4-bold text-light",
+				false: "text-body4 text-lightest border-transparent",
+			},
+		},
+		defaultVariants: {
+			active: false,
+		},
+	},
 );
 
 export type TabItemVariantsProps = VariantProps<typeof tabItemVariants>;
